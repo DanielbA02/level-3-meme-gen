b0bc4b97d152484d76c6f08ed197b3f563ae2e32
Daniel Alexander <db.alexander6902@gmail.com>
2022-09-07 12:43:32 -0400

    test

---
+++ meme-list-generator/my-app/src/components/Meme.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+export default function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    const [allMemes, setAllMemes] = React.useState([])
+
+    React.useEffect(() => {
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+        }
+        getMeme()
+    }, [])
+    
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
+    }
+
+}
